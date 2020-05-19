eb38859165957ff12698bd9662e1d9ff15dae6df
Ciaran Woodward <ciaranwoodward@gmail.com>
2020-05-19 16:33:15 +0100

    Modify the named configuration as required

--- _dns64_force.sh
+++ _dns64_force.sh
@@ -16,6 +16,9 @@ ADDR3=google.cn
 PV4ARG="-4 -c2"
 PV6ARG="-6 -c2"
 
+# Location of the named config file containing DNS64 (configured by ot-br)
+NAMED_CONF="/etc/bind/named.conf.options"
+
 # Detect IPv4 connectivity
 if { ping $PV4ARG $ADDR1 || ping $PV4ARG $ADDR2 || ping $PV4ARG $ADDR3 ; } 1>&2
 then
@@ -28,15 +31,38 @@ then
 	HAVE_V6=1
 fi
 
+# Change behaviour and print message depending on connectivity
 if [ "$HAVE_V4" -a "$HAVE_V6" ]
 then
 	echo "Good! Have both IPv4 and IPv6 connectivity!"
 elif [ "$HAVE_V4" ]
 then
 	echo "OK - Have IPv4 connectivity. IPv6 not detected, forcing DNS64"
+	FORCE_DNS64=1
 elif [ "$HAVE_V6" ]
 then
 	echo "Strange - Have IPv6 but no IPv4 connectivity, not tested!"
 else
 	echo "Error - No internet connectivity detected!"
+	die "no internet detected"
+fi
+
+# Make sure the named config file configured by ot-br exists
+if [ ! -f "$NAMED_CONF" ]
+then
+	die "$NAMED_CONF does not exist"
+fi
+
+# Apply the necessary configuration, toggling between the following two lines (non-forced & forced DNS64):
+# dns64 64:ff9b::/96 { clients { thread; }; recursive-only yes; };
+# dns64 64:ff9b::/96 { clients { thread; }; recursive-only yes; exclude { any; }; };
+if [ "$FORCE_DNS64" ]
+then
+	echo "Using forced DNS64"
+	sed -Ei 's/dns64 ([^\s]+) \{ clients \{ thread; \}; recursive-only yes; \};/dns64 \1 { clients { thread; }; recursive-only yes; exclude { any; }; };/g' $NAMED_CONF
+	grep 'dns64.*exclude { any; }' $NAMED_CONF || die "Failed to force DNS64"
+else
+	echo "Using default DNS64"
+	sed -Ei 's/dns64 ([^\s]+) \{ clients \{ thread; \}; recursive-only yes; exclude \{ any; \}; \};/dns64 \1 { clients { thread; }; recursive-only yes; };/g' $NAMED_CONF
+	grep 'dns64.*exclude { any; }' $NAMED_CONF && die "Failed to unforce DNS64"
 fi
